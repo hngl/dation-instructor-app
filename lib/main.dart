01f814bef38283b990327dd92d7b80f66f2e5d6f
Jurriaan van Hengel <15790266+hngl@users.noreply.github.com>
2018-06-03 12:07:23 +0200

    Added Drawer to HomePage as a main menu

--- lib/main.dart
+++ lib/main.dart
@@ -32,12 +32,81 @@ class MyApp extends StatelessWidget {
   }
 }
 
+class MenuItem {
+  String title;
+  IconData icon;
+  MenuItem(this.title, this.icon);
+}
+
 // Main layout and navigation
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+
+  final drawerItems = [
+    new MenuItem('Agenda', Icons.calendar_today),
+    new MenuItem('Leerlingen', Icons.people),
+    new MenuItem('Cursussen', Icons.bubble_chart)
+  ];
+
+  @override
+  State<StatefulWidget> createState() {
+    return new HomePageState();
+  }
+
+}
+
+class HomePageState extends State<HomePage> {
+  int _selectedDrawerIndex = 0;
+
+  _getDrawerItemWidget(int pos) {
+    switch (pos) {
+      case 0:
+        return new AgendaPage();
+      case 1:
+        return new StudentsListPage();
+      case 2:
+        return new CourseInstancesPage();
+
+      default:
+        return new Text("Error");
+    }
+  }
+
+  _onSelectItem(int index) {
+    setState(() => _selectedDrawerIndex = index);
+    Navigator.of(context).pop(); // close the drawer
+  }
+
   @override
   Widget build(BuildContext context) {
+    var drawerOptions = <Widget>[];
+    for (var i = 0; i < widget.drawerItems.length; i++) {
+      var d = widget.drawerItems[i];
+      drawerOptions.add(
+          new ListTile(
+            leading: new Icon(d.icon),
+            title: new Text(d.title),
+            selected: i == _selectedDrawerIndex,
+            onTap: () => _onSelectItem(i),
+          )
+      );
+    }
+
     return new Scaffold(
-      body: new CourseInstancesPage(),
+      appBar: new AppBar(
+        // here we display the title corresponding to the fragment
+        // you can instead choose to have a static title
+        title: new Text(widget.drawerItems[_selectedDrawerIndex].title),
+      ),
+      drawer: new Drawer(
+        child: new Column(
+          children: <Widget>[
+            new UserAccountsDrawerHeader(
+                accountName: new Text("Beheerder"), accountEmail: null),
+            new Column(children: drawerOptions)
+          ],
+        ),
+      ),
+      body: _getDrawerItemWidget(_selectedDrawerIndex),
       floatingActionButton: new FloatingActionButton(
           heroTag: 'experimentFAB',
           child: new Icon(Icons.play_arrow),
@@ -49,13 +118,25 @@ class HomePage extends StatelessWidget {
   }
 }
 
+class AgendaPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(child: new Text('Agenda'));
+  }
+}
+
+class StudentsListPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Center(child: new Text('Leerlingen'));
+  }
+
+}
+
 class CourseInstancesPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-      appBar: new AppBar(
-        title: new Text('Cursussen'),
-      ),
       body: new FutureBuilder(
           future: getCourseInstances(),
           builder: (BuildContext context, AsyncSnapshot<List> snapshot) {
@@ -87,7 +168,7 @@ class CourseInstancesPage extends StatelessWidget {
                 courseInstance.remainingAttendeeCapacity > 0
                     ? Icons.event_available
                     : Icons.event_busy,
-                color: Colors.purple),
+                color: Theme.of(context).primaryColor),
             title: new Text(courseInstance.name),
             subtitle: new Text(dateFormatter.format(courseInstance.startDate)),
             onTap: () {
