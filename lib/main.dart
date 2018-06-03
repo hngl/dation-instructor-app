2aa77a5075b4cb28bc766a2a9bfded459732a3af
Jurriaan van Hengel <15790266+hngl@users.noreply.github.com>
2018-06-03 11:23:59 +0200

    Split MyApp into ~, HomePage and CourseInstancesPage

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,18 @@ void main() {
   runApp(new MyApp());
 }
 
+void _doExperiment() {
+  var client = new DationWsClient('$wsHost');
+  client.setTenant(new Tenant(873, 'dation'));
+  client.setUser(new User(1, 'beheerder'));
+  client
+      .getAgendaBlocks(instructor: new Instructor(1), date: new DateTime.now())
+      .then((List blocks) => blocks.forEach((block) => print(block.toString())))
+      .catchError((e) => print(e.toString()));
+}
+
 class MyApp extends StatelessWidget {
+
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
@@ -18,33 +29,51 @@ class MyApp extends StatelessWidget {
         theme: new ThemeData(
           primaryColor: Colors.purple,
         ),
-        home: new Scaffold(
-          appBar: new AppBar(
-            title: new Text('Cursussen'),
-          ),
-          body: new FutureBuilder(
-              future: getCourseInstances(),
-              builder: (BuildContext context, AsyncSnapshot<List> snapshot) {
-                if (!snapshot.hasData)
-                  // Shows progress indicator until the data is load.
-                  return new MaterialApp(
-                      home: new Scaffold(
+        home: new HomePage()
+    );
+  }
+}
+
+// Main layout and navigation
+class HomePage extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return new CourseInstancesPage();
+  }
+}
+
+class CourseInstancesPage extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text('Cursussen'),
+      ),
+      body: new FutureBuilder(
+          future: getCourseInstances(),
+          builder: (BuildContext context, AsyncSnapshot<List> snapshot) {
+            if (!snapshot.hasData)
+              // Shows progress indicator until the data is load.
+              return new MaterialApp(
+                  home: new Scaffold(
                     body: new Center(
                       child: new CircularProgressIndicator(),
                     ),
                   ));
-                // Shows the real data with the data retrieved.
-                List courseInstances = snapshot.data;
-                return new ListView(
-                  children: createCourseInstanceSummaryWidgets(
-                      courseInstances, context),
-                );
-              }),
-          floatingActionButton: new FloatingActionButton(
-              child: new Icon(Icons.play_arrow),
-              backgroundColor: Colors.redAccent,
-              onPressed: _doExperiment),
-        ));
+            // Shows the real data with the data retrieved.
+            List courseInstances = snapshot.data;
+            return new ListView(
+              children: createCourseInstanceSummaryWidgets(
+                  courseInstances, context),
+            );
+          }),
+      floatingActionButton: new FloatingActionButton(
+          child: new Icon(Icons.play_arrow),
+          backgroundColor: Colors.redAccent,
+          onPressed: _doExperiment),
+    );
   }
 
   List<Widget> createCourseInstanceSummaryWidgets(
@@ -68,7 +97,7 @@ class MyApp extends StatelessWidget {
                   context,
                   new MaterialPageRoute(
                       builder: (BuildContext context) =>
-                          new CourseInstanceDetailWidget(courseInstance)));
+                      new CourseInstanceDetailPage(courseInstance)));
             });
         widgetList.add(listItem);
       }
@@ -76,21 +105,12 @@ class MyApp extends StatelessWidget {
     return widgetList;
   }
 
-  void _doExperiment() {
-    var client = new DationWsClient('$wsHost');
-    client.setTenant(new Tenant(873, 'dation'));
-    client.setUser(new User(1, 'beheerder'));
-    client
-        .getAgendaBlocks(instructor: new Instructor(1), date: new DateTime.now())
-        .then((List blocks) => blocks.forEach((block) => print(block.toString())))
-        .catchError((e) => print(e.toString()));
-  }
 }
 
-class CourseInstanceDetailWidget extends StatelessWidget {
+class CourseInstanceDetailPage extends StatelessWidget {
   final CourseInstance courseInstance;
 
-  CourseInstanceDetailWidget(this.courseInstance);
+  CourseInstanceDetailPage(this.courseInstance);
 
   @override
   Widget build(BuildContext context) {
