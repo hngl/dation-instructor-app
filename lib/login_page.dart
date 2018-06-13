cbee321ff68de2e5a494d6febf38a897337fa225
Jurriaan van Hengel <15790266+hngl@users.noreply.github.com>
2018-06-13 15:55:39 +0200

    Added Asset images, styling Login page

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -36,62 +36,80 @@ class _LoginPageState extends State<LoginPage> {
   Widget build(BuildContext context) {
     return Theme(
       data: ThemeData(
-          canvasColor: Theme.of(context).primaryColor,
-          brightness: Brightness.dark,
-          accentColor: Theme.of(context).accentColor,
-          splashColor: Theme.of(context).splashColor,
-          buttonColor: Theme.of(context).accentColor),
+        canvasColor: Theme.of(context).primaryColor,
+        brightness: Brightness.dark,
+        accentColor: Theme.of(context).accentColor,
+        splashColor: Theme.of(context).splashColor,
+        buttonColor: Colors.white,
+      ),
       child: Scaffold(
-        body: Center(
-          child: new SingleChildScrollView(
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: <Widget>[
-                Image.network(
-                  'http://www.dation.nl/wp-content/uploads/2018/04/elephant_white-300x257.png',
-                ),
-                Form(
-                  key: formKey,
-                  child: Padding(
-                    padding: EdgeInsets.all(30.0),
-                    child: Column(
-                      children: <Widget>[
-                        TextFormField(
-                          initialValue: 'dation',
-                          onSaved: (val) => _handle = val,
-                          decoration:
-                          InputDecoration(labelText: "Rijschool"),
-                        ),
-                        SizedBox(height: 12.0),
-                        TextFormField(
-                          initialValue: 'beheerder',
-                          onSaved: (val) => _username = val,
-                          decoration:
-                          InputDecoration(labelText: "Gebruikersnaam"),
-                        ),
-                        SizedBox(height: 12.0),
-                        TextFormField(
-                          initialValue: 'beheerder12',
-                          obscureText: true,
-                          onSaved: (val) => _password = val,
-                          decoration: InputDecoration(labelText: "Wachtwoord"),
-                        ),
-                        SizedBox(height: 36.0),
-                        RaisedButton(
-                          onPressed: _submit,
-                          child: Padding(
-                            padding: EdgeInsets.symmetric(
-                              vertical: 18.0,
-                              horizontal: 40.0,
-                            ),
-                            child: Text('Aanmelden'),
-                          ),
-                        ),
-                      ],
+        body: new Container(
+          decoration: new BoxDecoration(
+            image: new DecorationImage(
+              image: new AssetImage(
+                  'assets/graphics/adventure-agriculture-auto-754595.png'),
+              fit: BoxFit.cover,
+            ),
+          ),
+          child: new Padding(
+            padding: const EdgeInsets.all(50.0),
+            child: Center(
+              child: new SingleChildScrollView(
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    new Padding(
+                      padding: const EdgeInsets.all(25.0),
+                      child: Image.asset(
+                        'assets/graphics/logo_white.png',
+                      ),
                     ),
-                  ),
-                )
-              ],
+                    SizedBox(height: 18.0),
+                    Form(
+                      key: formKey,
+                      child: Column(
+                        children: <Widget>[
+                          TextFormField(
+                            initialValue: 'dation',
+                            onSaved: (val) => _handle = val,
+                            decoration: InputDecoration(labelText: "Rijschool"),
+                          ),
+                          SizedBox(height: 12.0),
+                          TextFormField(
+                            initialValue: 'beheerder',
+                            onSaved: (val) => _username = val,
+                            decoration:
+                                InputDecoration(labelText: "Gebruikersnaam"),
+                          ),
+                          SizedBox(height: 12.0),
+                          TextFormField(
+                            initialValue: 'beheerder12',
+                            obscureText: true,
+                            onSaved: (val) => _password = val,
+                            decoration:
+                                InputDecoration(labelText: "Wachtwoord"),
+                          ),
+                          SizedBox(height: 36.0),
+                          RaisedButton(
+                            onPressed: _submit,
+                            child: Padding(
+                              padding: EdgeInsets.symmetric(
+                                  vertical: 16.0, horizontal: 40.0),
+                              child: Text(
+                                'Aanmelden',
+                                style: TextStyle(
+                                    color: Colors.deepPurple[900], fontSize: 16.0),
+                              ),
+                            ),
+                            shape: new RoundedRectangleBorder(
+                                borderRadius: new BorderRadius.circular(20.0)),
+                          ),
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              ),
             ),
           ),
         ),
